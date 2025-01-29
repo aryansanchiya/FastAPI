eb977d7c52a64893e3481ca0229f26b4b1363e35
aryansanchiya <asanchiya@gmail.com>
2025-01-29 08:54:58 -0800

    Third Commit

--- books.py
+++ books.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -18,11 +19,22 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id : int
-    title : str
-    author : str
-    description : str
-    rating : int
+    id : Optional[int] = Field(description="Default id is not needed!", default=None) 
+    title : str = Field(min_length=3)
+    author : str = Field(min_length=1)
+    description : str = Field(min_length=1, max_length=100)
+    rating : int = Field(gt=0, lt=6)
+
+    # model_config = {
+    #     "json_schema_extra": {
+    #         "example" : {
+    #             "title" : "A new book",
+    #             "author" : "Add Author",
+    #             "description" : "Add Description",
+    #             "rating" : "Add Book Rating"
+    #         }
+    #     }
+    # }
 
 BOOKS = [
     Book(
@@ -70,4 +82,17 @@ async def read_all_books():
 @app.post("/create_book")
 async def create_book(book_request:BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    print(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book:Book):
+    # book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # print("IDD:",book.id)
+
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
+    
+    return book
