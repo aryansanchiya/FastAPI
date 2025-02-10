ba33747f0473f78e8a79063ccd643c01d2831ff2
aryansanchiya <asanchiya@gmail.com>
2025-02-10 08:48:06 -0800

    100225

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -10,13 +10,15 @@ class Book:
     author : str
     description : str
     rating : str
+    published_year : int
 
-    def __init__(self,id,title,author,description, rating):
+    def __init__(self,id,title,author,description, rating, published_year):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_year = published_year
 
 class BookRequest(BaseModel):
     id : Optional[int] = Field(description="Default id is not needed!", default=None) 
@@ -24,6 +26,7 @@ class BookRequest(BaseModel):
     author : str = Field(min_length=1)
     description : str = Field(min_length=1, max_length=100)
     rating : int = Field(gt=0, lt=6)
+    published_year : int = Field(min_length=2000, max_length=2025)
 
     # model_config = {
     #     "json_schema_extra": {
@@ -42,35 +45,40 @@ BOOKS = [
         title="To Kill a Mockingbird",
         author="Harper Lee",
         description="A novel about the serious issues of rape and racial inequality, told through the eyes of a young girl.",
-        rating=4
+        rating=4,
+        published_year=2023
     ),
     Book(
         id=2,
         title="1984",
         author="George Orwell",
         description="A dystopian novel that delves into themes of government surveillance and totalitarianism.",
-        rating=4
+        rating=4,
+        published_year=2020
     ),
     Book(
         id=3,
         title="Pride and Prejudice",
         author="Jane Austen",
         description="A classic romantic novel exploring love, reputation, and class in 19th-century England.",
-        rating=4
+        rating=4,
+        published_year=2019
     ),
     Book(
         id=4,
         title="The Great Gatsby",
         author="F. Scott Fitzgerald",
         description="A critique of the American Dream through the tragic story of Jay Gatsby and his unfulfilled love for Daisy Buchanan.",
-        rating=4
+        rating=4,
+        published_year=2005
     ),
     Book(
         id=5,
         title="The Catcher in the Rye",
         author="J.D. Salinger",
         description="A story of teenage rebellion and alienation, told by the iconic narrator Holden Caulfield.",
-        rating=4.4
+        rating=4.4,
+        published_year=2002
     )
 ]
 
@@ -85,7 +93,31 @@ async def create_book(book_request:BookRequest):
     print(new_book)
     BOOKS.append(find_book_id(new_book))
 
+@app.get("/get_book/{book_id}")
+async def get_book(book_id:int = Path(gt=0)):       #Parameter Validation with Path
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
 
+@app.put("/update_book/")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+@app.delete("/delete_book/{book_id}")
+async def delete_book(book_id:int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+@app.get("/get_publish/{publish_year}")
+async def get_publish_book(year:int):
+    for book in BOOKS:
+        if book.published_year == year:
+            return book
+            
 def find_book_id(book:Book):
     # book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     # print("IDD:",book.id)
