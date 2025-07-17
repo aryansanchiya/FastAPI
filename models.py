7fea2dad78890907585a3b53126511907e8447cc
meet7056 <gohelmeet1212@gmail.com>
2025-07-18 00:12:45 +0530

    17-07-25

---
+++ models.py
@@ -0,0 +1,9 @@
+from database import engine
+from sqlalchemy import Integer, Column, String, Boolean
+class Todos(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
